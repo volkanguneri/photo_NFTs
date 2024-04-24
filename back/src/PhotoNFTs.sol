3203908ec8d7f46bf712d903460926ef317586fa
root <vocci@hotmail.com>
2024-04-24 17:14:00 +0200

    added PhotoNFT contract, used  erc721

--- back/src/PhotoNFTs.sol
+++ back/src/PhotoNFTs.sol
@@ -1,3 +1,24 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PhotoNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("PhotoNFT", "PNFT") Ownable(msg.sender) {}
+
+    function mintPNFT(
+        address to,
+        string memory tokenURI
+    ) public onlyOwner returns (uint256) {
+        _tokenIdCounter++;
+        uint256 newTokenId = _tokenIdCounter;
+        _mint(to, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
+    }
+}
