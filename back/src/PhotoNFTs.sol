f7210efcf05fccd5513cdcb1bd33be14ed1be5da
root <vocci@hotmail.com>
2024-04-25 15:14:01 +0200

    updated PhotoNFTs.sol

--- back/src/PhotoNFTs.sol
+++ back/src/PhotoNFTs.sol
@@ -8,14 +8,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract PhotoNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
 
+    struct PhotoNFTs {
+        string photographer;
+        string city;
+        string vieW;
+        uint256 year;
+    }
+
+    PhotoNFTs[] photoNFTs;
+
     constructor() ERC721("PhotoNFT", "PNFT") Ownable(msg.sender) {}
 
     function mintPNFT(
         address to,
-        string memory tokenURI
-    ) public onlyOwner returns (uint256) {
+        string memory tokenURI,
+        string memory _photographer,
+        string memory _city,
+        string memory _vieW,
+        uint _year
+    ) external onlyOwner returns (uint256) {
         _tokenIdCounter++;
         uint256 newTokenId = _tokenIdCounter;
+        photoNFTs.push(PhotoNFTs(_photographer, _city, _vieW, _year));
+
         _mint(to, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
 
