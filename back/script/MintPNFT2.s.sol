81f421dc36fb8e640724b0edbf9fc99ea4a6df2b
root <vocci@hotmail.com>
2024-04-29 15:56:35 +0200

    updated metadata

--- back/script/MintPNFT2.s.sol
+++ back/script/MintPNFT2.s.sol
@@ -5,24 +5,24 @@ pragma solidity 0.8.25;
 import {Script} from "forge-std/Script.sol";
 import {PhotoNFT} from "../src/PhotoNFT.sol";
 
-contract MintPNFT is Script {
+contract MintPNFT2 is Script {
+    // contract to interact with
+    PhotoNFT public photoNFT =
+        PhotoNFT(0x2f4e9E46F60e7C005E4F2D586af033FaA85bf9CF);
+
     function run() external returns (uint256) {
         vm.startBroadcast();
-        PhotoNFT photoNFT = new PhotoNFT();
-        vm.stopBroadcast();
 
-        // Parameters of NFT to mint
+        // NFT parameters to mint
         address to = msg.sender;
         string
-            // memory tokenURI = "https://bafybeickdk6lqkpi3ehcpmaxweoopay7issorcjioksro4dknbx47m2fgm.ipfs.nftstorage.link/1.json";
-            memory tokenURI = "ipfs://bafybeickdk6lqkpi3ehcpmaxweoopay7issorcjioksro4dknbx47m2fgm/1.json";
+            memory tokenURI = "ipfs://bafybeibo32mw4iurh3yfzahximedyy25rif3oczttsuedkkfy6bl23c6ei.ipfs.nftstorage.link/1.json";
         string memory photographer = "Volkan Guneri";
         string memory city = "Athenes";
         string memory vieW = "Acropolis from rocks";
         uint256 year = 2024;
 
-        // NFT mint
-        vm.startBroadcast();
+        // NFT creation
         uint256 newTokenId = photoNFT.mintPNFT(
             to,
             tokenURI,
@@ -31,6 +31,7 @@ contract MintPNFT is Script {
             vieW,
             year
         );
+
         vm.stopBroadcast();
         return newTokenId;
     }
